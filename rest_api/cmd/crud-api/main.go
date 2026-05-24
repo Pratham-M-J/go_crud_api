4ab25e039a9f967261dc41d9a9bae5e465c17cbd
Pratham-M-J <prathammj2005@gmail.com>
2026-05-24 21:34:32 +0530

    graceful shutdown - goroutines and context

--- rest_api/cmd/crud-api/main.go
+++ rest_api/cmd/crud-api/main.go
@@ -4,32 +4,65 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"           // for os.Signal
+	"os/signal"    // listen for OS shutdown signals
+	"syscall"      // SIGTERM/SIGINT
+	"log/slog"
+	"context"
+	"time"         // timeout duration
+
 	"github.com/Pratham-M-J/crud_api/internal/config"
 )
 
 func main() {
 	fmt.Println("Welcome to crud api")
-	//load config
-	cfg := config.MustLoad()
-	//database setup
-	//setup router
-	router := http.NewServeMux()
 
-	router.HandleFunc("GET /home", func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("Welcome to CRUD API"))
+	cfg := config.MustLoad() // load YAML config into struct
+
+	router := http.NewServeMux() // create request router
+
+	router.HandleFunc("GET /home", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to CRUD API")) // response for GET /home
 	})
-	//setup server
+
 	server := http.Server{
-		Addr: cfg.Addr,
-		Handler: router,
-	}
-	fmt.Printf("Server Started %s", cfg.HTTPServer.Addr)
-
-	err := server.ListenAndServe()
-
-	if err != nil{
-		log.Fatal("failed to start server")
+		Addr:    cfg.Addr, // server port/address from config
+		Handler: router,   // attach router to server
 	}
 
-	
+	slog.Info("Server Started", slog.String("address",cfg.Addr))
+
+	done := make(chan os.Signal, 1) // channel waits for shutdown signal
+
+	signal.Notify(
+		done,
+		os.Interrupt,   // Ctrl+C
+		syscall.SIGINT, // interrupt signal
+		syscall.SIGTERM,// termination signal (docker/k8s)
+	)
+
+	go func() {
+		err := server.ListenAndServe() // start server in background
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatal("failed to start server")
+		}
+	}()
+
+	<-done // block until signal received
+
+	slog.Info("Shutting down the Server")
+
+	ctx, cancel := context.WithTimeout( //returns 2 values: ctx → the context object, cancel → a function 
+		context.Background(),
+		5*time.Second, // allow 5 sec for graceful shutdown
+	)
+	defer cancel()
+
+	err := server.Shutdown(ctx) // stop server gracefully
+	if err != nil {
+		slog.Error("failed to shutdown server")
+		log.Fatal(err)
+	}
+
+	slog.Info("Server shutdown successfully")
 }
